d6d47e0d029d846494932292812ff55f8d8c55fd
Yashira <yashira.dev@gmail.com>
2022-02-26 14:41:16 -0800

    create about.ejs file, add content, including partials, to about and contact files, add routes to app.js

--- app.js
+++ app.js
@@ -5,24 +5,34 @@ const ejs = require("ejs");
 
 app.set("view engine", "ejs");
 
-const homeStartingContent =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae asperiores impedit dolor ea, commodi reiciendis inventore id? Quos deleniti, non illum aperiam odit ipsa odio totam voluptates, delectus debitis voluptatibus!" 
+const homeStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae asperiores impedit dolor ea, commodi reiciendis inventore id? Quos deleniti, non illum aperiam odit ipsa odio totam voluptates, delectus debitis voluptatibus!" 
 
 const aboutContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae asperiores impedit dolor ea, commodi reiciendis inventore id? Quos deleniti, non illum aperiam odit ipsa odio totam voluptates, delectus debitis voluptatibus!"
 
-const contentContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae asperiores impedit dolor ea, commodi reiciendis inventore id? Quos deleniti, non illum aperiam odit ipsa odio totam voluptates, delectus debitis voluptatibus!"
+const contactContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae asperiores impedit dolor ea, commodi reiciendis inventore id? Quos deleniti, non illum aperiam odit ipsa odio totam voluptates, delectus debitis voluptatibus!"
 
 app.use(bodyParser.urlencoded({extended: true}));
 //static files
 app.use(express.static("public"));
 
-//home
+
 app.get("/", (req, res)=>{
     res.render("home", {
         homeStartingContent : homeStartingContent
     });
 });
 
+app.get("/about", (req, res)=>{
+    res.render("about", {
+        aboutContent : aboutContent
+    });
+});
+
+app.get("/contact", (req, res)=>{
+    res.render("contact", {
+        contactContent : contactContent
+    });
+});
 
 app.listen(3000, ()=>{
 console.log("Server is listening on Port 3000")
